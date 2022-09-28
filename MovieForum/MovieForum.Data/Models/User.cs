2b853c561a1a75a8ee345376b9e3c0b2357d15ab
Margarita Balimezova <maggie4balimezova@gmail.com>
2022-09-28 12:28:29 +0300

    Changed User Attributes

--- MovieForum/MovieForum.Data/Models/User.cs
+++ MovieForum/MovieForum.Data/Models/User.cs
@@ -9,27 +9,34 @@ namespace MovieForum.Data.Models
     internal class User : IDeletable
     {
         public int Id { get; set; }
-        
-        [MinLength(3)]
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 4, ErrorMessage = "Username length must be greater than {1} character")]
         public string Username { get; set; }
 
-        [MinLength(4), MaxLength(32)]
+        [Required]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "First Name must be greater between {1} and {0} character")]
         public string FirstName { get; set; }
-        
-        [MinLength(4), MaxLength(32)]
+
+        [Required]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Last Name must be greater between {1} and {0} character")]
         public string LastName { get; set; }
 
-        [MinLength(8)]
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 8, ErrorMessage = "Password length must be greater than {1} symbols")]
         public string Password { get; set; }
 
+        [Required]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
+        [Required]
         public virtual Role Role { get; set; }
 
         [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public bool IsDeleted { get ; set; }
         
         public DateTime? DeletedOn { get; set; }
