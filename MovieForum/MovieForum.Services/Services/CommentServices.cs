164379db4f0419e94f9638af5bb1cddc28cce7e8
Radoslav Berov <rado561.nbr@gmail.com>
2022-09-29 22:45:59 +0300

    Implemented:  ICommentServices CommentDTO CommentDTOMapper

---
+++ MovieForum/MovieForum.Services/Services/CommentServices.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieForum.Data;
+using MovieForum.Services.Mappers;
+using MovieForum.Services.Services.DTOModels;
+using MovieForum.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieForum.Services.Services
+{
+    public class CommentServices : ICommentServices
+    {
+        private readonly MovieForumContext data;
+
+        public CommentServices(MovieForumContext forumData)
+        {
+            this.data = forumData;
+        }
+        public async Task<CommentDTO> DeleteAsync(int id)
+        {
+            var comment = await data.Comments.Include(x => x.Author).Include(x => x.Movie).FirstOrDefaultAsync(x => x.Id == id);
+
+            var commentDto = comment.GetDTO();
+
+            comment.DeletedOn = DateTime.Now;
+            data.Comments.Remove(comment);
+            await data.SaveChangesAsync();
+
+            return commentDto;                
+        }
+
+        public async Task<CommentDTO> DislikeComment(int commentId)
+        {
+            var comment = await data.Comments.Include(x => x.Movie).Include(x => x.Author).FirstOrDefaultAsync(x => x.Id == commentId);
+
+            comment.DisLikesCount++;
+
+            var commentDTO = comment.GetDTO();
+
+            return commentDTO;
+        }
+
+        public async Task<IEnumerable<CommentDTO>> GetAsync()
+        {
+            var comments = await data.Comments.Include(x => x.Movie).Include(x => x.Author).Select(x=>x.GetDTO()).ToListAsync();
+
+            return comments;
+        }
+
+        public async Task<CommentDTO> LikeComment(int commentId)
+        {
+            var comment = await data.Comments.Include(x => x.Movie).Include(x => x.Author).FirstOrDefaultAsync(x => x.Id == commentId);
+
+            comment.LikesCount++;
+
+            var commentDTO = comment.GetDTO();
+
+            return commentDTO;
+        }
+
+        public async Task<CommentDTO> PostAsync(CommentDTO obj)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<CommentDTO> UpdateAsync(int id, CommentDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieForum/MovieForum.Services/Services/DTOModels/CommentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieForum.Services.Services.DTOModels
+{
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int? AuthorId { get; set; }
+
+        public string AuthorUsername { get; set; }
+
+        public int? MovieId { get; set; }
+
+        public DateTime? PostedOn { get; set; }
+    }
+}
+}
