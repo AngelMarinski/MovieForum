fd1e22b5582a8f22321f088c225a5923a31af830
Radoslav Berov <rado561.nbr@gmail.com>
2022-10-26 15:37:07 +0300

    Comment View and Controller implemented Bootstrap added with css and donts

---
+++ MovieForum/MovieForum/Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieForum.Services.DTOModels;
+using MovieForum.Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieForum.Web.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentServices services;
+
+        public CommentController(ICommentServices services)
+        {
+            this.services = services;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var comments = await this.services.GetAsync();
+            return View(comments);
+        }
+
+        [HttpGet]
+		public async Task<IActionResult> CreateAsync()
+		{
+			var com = new CommentDTO();
+
+			return this.View(com);
+		}
+
+		
+
+	}
+}
