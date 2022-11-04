d4d18bf9e740b3a659e4b1b630328b208e499b3d
Radoslav Berov <rado561.nbr@gmail.com>
2022-11-04 13:50:50 +0200

    Movie pics size formatted Movies search button works Pages titles placed min title constraint changed to 2 max movie content contraint changed to 2000

--- MovieForum/MovieForum.Services/Helpers/Constants.cs
+++ MovieForum/MovieForum.Services/Helpers/Constants.cs
@@ -7,10 +7,10 @@ namespace MovieForum.Services.Helpers
     public static class Constants
     {
         //Constraints
-        public const int MOVIE_TITLE_MIN_LENGHT = 16;
+        public const int MOVIE_TITLE_MIN_LENGHT = 2;
         public const int MOVIE_TITLE_MAX_LENGHT = 64;
         public const int MOVIE_CONTENT_MIN_LENGHT = 32;
-        public const int MOVIE_CONTENT_MAX_LENGHT = 8192;
+        public const int MOVIE_CONTENT_MAX_LENGHT = 2000;
 
         public const int USER_FIRSTNAME_MIN_LENGTH = 4;
         public const int USER_FIRSTNAME_MAX_LENGTH = 32;
